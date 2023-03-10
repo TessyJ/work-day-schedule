703f587c1b360e06809f823154067f48321719ef
Theresa <theresaolusoga@Bolajis-MacBook-Pro.local>
2023-03-10 18:18:48 -0500

    Generate schedule field with jquery

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,24 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    //
+
+    // Generate schedule fields from here
+    // Create a time block for each hour of the day
+    for (var hour = 0; hour < 24; hour++) {
+      var timeBlock = $('<div>').addClass('row time-block ');
+      var hourColumn = $('<div>').addClass('col-2 col-md-1 hour text-center py-3').text(hour);
+      var descriptionColumn = $('<textarea row="3">').addClass('col-8 col-md-10 description');
+      var saveButton = $('<button>').addClass('btn saveBtn col-2 col-md-1').html('<i class="fas fa-save" aria-hidden="true"></i>');
+
+      // Append the columns to the time block and the time block to the container
+      timeBlock.append(hourColumn, descriptionColumn, saveButton);
+      $('.container-lg').append(timeBlock);
+
+    }
+
+
+
+    
     // TODO: Add code to apply the past, present, or future class to each time
     // block by comparing the id to the current hour. HINTS: How can the id
     // attribute of each time-block be used to conditionally add or remove the
