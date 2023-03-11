ad674367faaf8890f8b2dbf5a05115b04888ef1c
Theresa <theresaolusoga@Bolajis-MacBook-Pro.local>
2023-03-10 19:08:44 -0500

    Changed text color to black and format hour to AM and PM

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,7 @@ $(function () {
     // Create a time block for each hour of the day
     for (var hour = 9; hour < 24; hour++) {
       var timeBlock = $('<div>').addClass('row time-block ');
-      var hourColumn = $('<div>').addClass('col-2 col-md-1 hour text-center py-3').text(hour);
+      var hourColumn = $('<div>').addClass('col-2 col-md-1 hour text-center py-3').text(checkHour(hour));
       var descriptionColumn = $('<textarea row="3">').addClass('col-8 col-md-10 description');
       var saveButton = $('<button>').addClass('btn saveBtn col-2 col-md-1').html('<i class="fas fa-save" aria-hidden="true"></i>');
 
@@ -49,19 +49,20 @@ $(function () {
       var event = events.find(function(event) {
         return event.hour === hour;
       });
-      
+
+      console.log(event)
       if (event) {
         descriptionColumn.val(event.text);
        }
 
     }
-
-     
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
+    
+    function checkHour(hour){
+      return dayjs().hour(hour).format('hA')
+      // return hour
+    }
+      
+    // display the current date in the header of the page.
     var currentDate = dayjs().format('dddd, MMMM D');
     $('#currentDay').text(currentDate);
   });
